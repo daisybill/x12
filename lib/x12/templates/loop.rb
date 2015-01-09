22f72761c9e7b356305b4ed45675de6b9b9b16fd
Eugene Surzhko <eugenesu@active-bridge.com>
2015-01-09 14:54:33 +0200

    added loop template

--- lib/x12/templates/loop.rb
+++ lib/x12/templates/loop.rb
@@ -1,7 +1,14 @@
 module X12
   module Templates
     class Loop < X12::Templates::Base
+      options :required?, :range
 
+      def self.parse_options(node)
+        {
+          required: X12::Attributes.boolean(node, :required),
+          range: X12::Attributes.range(node, :min, :max)
+        }
+      end
     end
   end
 end
